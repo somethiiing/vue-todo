2c8f6bc9e4773911ce7554392ccb02f4e2ca08c7
somethiiing <hello@wilsonyu.io>
2017-07-06 15:53:22 -0400

    implement refocus + reset button

--- src/components/CreateTodo.vue
+++ src/components/CreateTodo.vue
@@ -5,13 +5,13 @@
         <div class='ui form'>
           <form v-on:submit.prevent v-on:submit="sendForm()" >
             <div class='field'>
-              <input placeholder="Title" v-focus v-model="note.title" type='text' ref='title' defaultValue="">
+              <input placeholder="Title" id="textInput" v-model="note.title" type='text' ref='title' defaultValue="">
             </div>
             <div class='field'>
               <input placeholder="Project" v-model="note.project" type='text' ref='project' defaultValue="">
             </div>
             <div class='ui two button attached buttons'>
-              <button class='ui basic blue button' type="submit">
+              <button :disabled="note.title.length < 1 || note.project.length < 1" class='ui basic blue button' type="submit">
                 Create
               </button>
               <button class='ui basic red button' v-on:click="reset()">
@@ -33,23 +33,16 @@ export default {
         title: '',
         project: '',
         done: false
-      },
-      isCreating: false,
+      }
     };
   },
   methods: {
-    openForm() {
-      this.isCreating = true;
-    },
-    closeForm() {
-      this.isCreating = false;
-    },
     sendForm() {
-      if (this.note.title.length > 0 && this.note.project.length > 0) {
+      if (this.note.title.length > 0 || this.note.project.length > 0) {
         this.$emit('create-todo', this.note );
         this.reset();
+        document.getElementById("textInput").focus();
       }
-      this.isCreating = false;
     },
     reset() {
       this.note = {title: '', project: '', done: false};
